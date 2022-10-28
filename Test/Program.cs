07777c4a6ad34390c46d69c4051c4d17195eacc9
Junya1017 <20205013@g.aut.ac.jp>
2022-10-28 16:11:06 +0900

    try文追加

--- Test/Program.cs
+++ Test/Program.cs
@@ -1,11 +1,10 @@
 ﻿using System;
-using PCSC;
-using PCSC.Iso7816;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FelicaLib;
 
 namespace Test
 {
@@ -13,7 +12,17 @@ namespace Test
     {
         static void Main(string[] args)
         {
-
-        } 
+			try
+			{
+				using (Felica f = new Felica())
+				{
+					readNanaco(f);
+				}
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex.Message);
+			}
+		} 
     }
 }
