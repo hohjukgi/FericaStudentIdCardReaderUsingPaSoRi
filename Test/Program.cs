25d6b8c41ab3177c994a54c43662d437d88244b3
hohjukgi <20205073@g.aut.ac.jp>
2022-10-28 16:31:13 +0900

    ・とりあえず

--- Test/Program.cs
+++ Test/Program.cs
@@ -1,74 +1,75 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using FelicaLib;
 
-namespace Test
+namespace FelicaLib
 {
-    class Program
+    public class Nanaco
     {
-        static void Main(string[] args)
+        public static void Main()
         {
-			try
-			{
-				using (Felica f = new Felica())
-				{
-					readCard(f);
-				}
-			}
-			catch (Exception ex)
-			{
-				Console.WriteLine(ex.Message);
-			}
-		}
-
-        private static void readCard(Felica f)
-        {
-            int i, j, k;
-            f.Polling((int)SystemCode.Any);
-
-            Console.Write("# IDm: ");
-            hexdump(f.IDm(), 8);
-            Console.Write("\n");
-            Console.Write("# PMm: ");
-            hexdump(f.PMm(), 8);
-            Console.Write("\n\n");
-
-            felicat felicaf2 = new felicat();
-            felicat felicaf1 = f.felica_enum_systemcode();
-
-            for (i = 0; i < felicaf1.num_system_code; i++)
+            try
             {
-                int syscode = ((felicaf1.system_code[i]) >> 8) & 0xff | ((felicaf1.system_code[i]) << 8) & 0xff00;
-                Console.Write("# System code: {0:x4}\n", syscode);
-                felicaf2 = f.felica_enum_service(syscode);
-
-                Console.Write("# Number of area = {0}\n", felicaf2.num_area_code);
-                for (j = 0; j < felicaf2.num_area_code; j++)
+                using (Felica f = new Felica())
                 {
-                    Console.Write("# Area: {0:x4} - {1:x4}\n", felicaf2.area_code[j], felicaf2.end_service_code[j]);
+                    readNanaco(f);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private static void readNanaco(Felica f)
+        {
+            f.Polling((int)SystemCode.Common);
+            byte[] data = f.ReadWithoutEncryption(0x558b, 0);
+            if (data == null)
+            {
+                throw new Exception("nanaco ID が読み取れません");
+            }
+            Console.Write("Nanaco ID = ");
+            for (int i = 0; i < 8; i++)
+            {
+                Console.Write(data[i].ToString("X2"));
+            }
+            Console.Write("\n");
+
+            for (int i = 0; ; i++)
+            {
+                data = f.ReadWithoutEncryption(0x564f, i);
+                if (data == null) break;
+
+                switch (data[0])
+                {
+                    case 0x47:
+                    default:
+                        Console.Write("支払     ");
+                        break;
+                    case 0x6f:
+                        Console.Write("チャージ ");
+                        break;
                 }
 
-                Console.Write("# Number of service code = {0}\n", felicaf2.num_service_code);
-                for (j = 0; j < felicaf2.num_service_code; j++)
-                {
-                    ushort service = felicaf2.service_code[j];
-                    printserviceinfo(service);
+                int value = (data[9] << 24) + (data[10] << 16) + (data[11] << 8) + data[12];
+                int year = (value >> 21) + 2000;
+                int month = (value >> 17) & 0xf;
+                int date = (value >> 12) & 0x1f;
+                int hour = (value >> 6) & 0x3f;
+                int min = value & 0x3f;
 
-                    for (k = 0; k < 255; k++)
-                    {
-                        data = f.ReadWithoutEncryption((int)felicaf2.service_code[j], k);
-                        if (data == null) break;
+                Console.Write("{0}/{1:D2}/{2:D2} {3:D2}:{4:D2}", year, month, date, hour, min);
 
-                        Console.Write("{0:x4}:{1:x4} ", (int)felicaf2.service_code[j], k);
-                        hexdump(data, 16);
-                        Console.Write("\n");
-                    }
-                }
-                Console.Write("\n");
+                value = (data[1] << 24) + (data[2] << 16) + (data[3] << 8) + data[4];
+                Console.Write("  金額 {0,6}円", value);
+
+                value = (data[5] << 24) + (data[6] << 16) + (data[7] << 8) + data[8];
+                Console.Write("  残高 {0,6}円", value);
+
+                value = (data[13] << 8) + data[14];
+                Console.WriteLine("  連番 {0}", value);
             }
         }
     }
