19bafd669ae8aefea12cb75dd6f6da3c5a19b63a
Junya1017 <20205013@g.aut.ac.jp>
2022-11-04 14:18:11 +0900

    安定版

--- Test/Program.cs
+++ Test/Program.cs
@@ -14,7 +14,8 @@ namespace FelicaLib
                 using (Felica f = new Felica())
                 {
                     f.Polling((int)SystemCode.Common);
-                    Console.WriteLine(f.IDm());
+                    //Console.WriteLine(f.IDm() BitCoverter);
+                    Console.WriteLine(BitConverter.ToString(f.IDm()));
                 }
                 while (true) ;
             }
